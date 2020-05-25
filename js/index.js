600256f6450ed14969a274ff8358c028fa53b767
Federico Fusco <40967674+federicofusco@users.noreply.github.com>
2020-05-25 12:52:25 +0200

    Major Changes
    - Fixed Visual bug
    - Added various animations
    - Completed Language list

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function hoverDiscordWidget() {
+  document.getElementById("findme-widget").style.height = "15%";
+  document.getElementById("discord-widget").style.height = "105%";
+}
+
+function leaveDiscordWidget() {
+  document.getElementById("findme-widget").style.height = "20%";
+  document.getElementById("discord-widget").style.height = "100%";
+}
+
+function hoverFindMeWidget() {
+  document.getElementById("findme-widget").style.height = "25%";
+  document.getElementById("discord-widget").style.height = "95%";
+}
+
+function leaveFindMeWidget() {
+  document.getElementById("findme-widget").style.height = "20%";
+  document.getElementById("discord-widget").style.height = "100%";
+}
